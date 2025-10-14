9aa809eb17c48761ddf0d85e11a3329c38da75b3
Raphaël Colin <raphael.colin68@gmail.com>
2025-10-15 00:01:41 +0200

    fix: tabs and space indentation in dialogue controller

--- autoloads/dialogue_controller/dialogue_controller.gd
+++ autoloads/dialogue_controller/dialogue_controller.gd
@@ -9,23 +9,23 @@ var dialogue_queue: Array[String] # A queue of dialogue lines
 var play_count: int = 0 # Number of dialogue lines to play
 
 func _ready() -> void:
-  ui_dialogue.dialogue_finished.connect(_on_dialogue_finished)
+	ui_dialogue.dialogue_finished.connect(_on_dialogue_finished)
 
 # Forward the provided dialogue to the controller
 func _play_dialogue(text: String):
-  ui_dialogue.display_dialogue(text)
+	ui_dialogue.display_dialogue(text)
 
 # Signal handler for when dialogue is done displaying
 func _on_dialogue_finished() -> void:
-  if dialogue_queue.is_empty() or play_count == 0:
-    return
-  else:
-    _play_dialogue(dialogue_queue.pop_front())
+	if dialogue_queue.is_empty() or play_count == 0:
+		return
+	else:
+		_play_dialogue(dialogue_queue.pop_front())
 
 # Push dialogue in the dialogue queue
 func push_dialogue(text: String) -> void:
-  dialogue_queue.push_back(text)
+	dialogue_queue.push_back(text)
 
 func play_queue() -> void:
-  play_count = dialogue_queue.size()
-  _play_dialogue(dialogue_queue.pop_front())
+	play_count = dialogue_queue.size()
+	_play_dialogue(dialogue_queue.pop_front())
