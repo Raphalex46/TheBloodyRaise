9ed46d87b147ea683a0505bcc672359efb419750
Nawreua <erwan.auer@inria.fr>
2025-10-16 00:41:58 +0200

    browser: add interactions to browser singleton

--- autoloads/save_cookie_controller/save_cookie_controller.gd
+++ autoloads/save_cookie_controller/save_cookie_controller.gd
@@ -14,6 +14,40 @@ func retrieve_savepoint() -> String:
 	else:
 		return savepoint
 
+func set_savepoint(new_savepoint: String):
+	if stub_mode:
+		stub_savepoint = new_savepoint
+	else:
+		savepoint = new_savepoint
+		var document: JavaScriptObject = JavaScriptBridge.get_interface(&"document")
+		if document != null:
+			# Set the new savepoint cookie
+			document.cookie = "%s=%s; SameSite=None; Secure" % [SAVEPOINT_NAME, new_savepoint]
+
+func reload_page():
+	if stub_mode:
+		# A workaround over the hard browser reload
+		get_tree().reload_current_scene()
+	else:
+		var window: JavaScriptObject = JavaScriptBridge.get_interface(&"window")
+		if window != null:
+			window.location.reload()
+
+func open_page(url: String):
+	if not stub_mode:
+		var window: JavaScriptObject = JavaScriptBridge.get_interface(&"window")
+		if window != null:
+			window.open(url, "_blank")
+
+func download_file(content: String, filename: String):
+	if stub_mode:
+		FileAccess.open(filename, FileAccess.WRITE).store_string(content)
+	else:
+		JavaScriptBridge.download_buffer(
+			content.to_utf8_buffer(),
+			filename
+		)
+
 func _ready() -> void:
 	# Retrieve cookie information
 	var document: JavaScriptObject = JavaScriptBridge.get_interface(&"document")
