23f301435a610b303cf63aed17b3e635d6b18c6f
Nawreua <erwan.auer@inria.fr>
2025-10-16 22:11:29 +0200

    player: add footsteps

--- entities/characters/player/player_controller.gd
+++ entities/characters/player/player_controller.gd
@@ -113,6 +113,15 @@ func _walk(delta: float) -> Vector3:
 	# Forward is always the direction the camera is pointing towards
 	var _forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
 	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	if walk_dir != Vector3.ZERO:
+		if randi() % 8 == 0:
+			if not $Footstep1.playing:
+				$Footstep1.pitch_scale = randf_range(1.0, 1.05)
+				$Footstep1.play()
+		elif randi() % 8 == 0:
+			if not $Footstep2.playing:
+				$Footstep1.pitch_scale = randf_range(1.0, 1.05)
+				$Footstep2.play()
 	walk_vel = walk_vel.move_toward(walk_dir * speed, acceleration * delta)
 	return walk_vel
 
